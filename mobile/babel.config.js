70b5079c7e01eee3cd8dc871894fb2576ab1776c
Albert Nikanorov <alikatgh@users.noreply.github.com>
2026-02-24 00:40:49 +0800

    fix: resolve NativeWind v4 babel configuration crash

--- mobile/babel.config.js
+++ mobile/babel.config.js
@@ -1,7 +1,10 @@
 module.exports = function (api) {
-    const isTest = api.env('test');
+    api.cache(true);
     return {
-        presets: ['babel-preset-expo'],
-        plugins: isTest ? [] : ['nativewind/babel'],
+        presets: [
+            ['babel-preset-expo', { jsxImportSource: 'nativewind' }],
+            'nativewind/babel'
+        ],
+        plugins: [],
     };
 };
